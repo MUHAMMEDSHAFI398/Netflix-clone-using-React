64bef63ff0fcf8ab7138a1bf90af31d6f57e8211
MUHAMMEDSHAFI398 <shafimuhammed398@gmail.com>
2023-01-04 09:41:17 +0530

    some more movie category added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import NavBar from './Components/NavBar/NavBar';
 import './App.css'
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
-import {orginals,action} from './urls'
+import {Documentaries,HorrorMovies,RomanceMovies,action,trending} from './urls'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RowPost url={orginals} title='Netflix Orginals' />
+      <RowPost url={trending} title="trending"  /> 
+      <RowPost url={RomanceMovies} title="RomanceMovies" isSmall />
       <RowPost url={action} title="Action" isSmall />
+      <RowPost url={HorrorMovies} title="HorrorMovies" isSmall />
+      <RowPost url={Documentaries} title="Documentaries" isSmall />
+
     </div>
   );
 }
